02e78941a1f972e2d174c46c4dc0d46edaf4558e
hamdal <hameedayomide@gmail.com>
2021-06-08 16:26:45 +0100

    removed tests

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,4 @@
 jQuery(document).ready(function($) {
-
-    window.alert(window.origin.replace('www.', '').split('.').length);
-
     $('.scrollup').click(function() {
         $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
